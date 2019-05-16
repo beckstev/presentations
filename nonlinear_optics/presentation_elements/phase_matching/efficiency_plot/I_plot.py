a858611d8b2c7398ebe257636c8664fc2785f6da
beckstev <beckstev@web.de>
2019-05-16 17:08:06 +0200

    added png of efficiency

--- nonlinear_optics/presentation_elements/phase_matching/efficiency_plot/I_plot.py
+++ nonlinear_optics/presentation_elements/phase_matching/efficiency_plot/I_plot.py
@@ -6,7 +6,7 @@ def I(delta_k):
     return (np.sin( 2 * delta_k))**2 / ( 2 *delta_k )**2
 
 
-Dk = np.linspace(-2*np.pi, 2*np.pi, 1000)
+Dk = np.linspace(-2*np.pi, 2*np.pi, 1e5)
 
 
 plt.figure(figsize=(16, 9))
@@ -14,4 +14,4 @@ plt.plot(Dk, I(Dk))
 plt.ylabel(r'$\Gamma \, / \, \mathrm{a.u.} $')
 plt.xlabel(r'$\Delta k \, / \, \mathrm{a.u.} $')
 
-plt.savefig('./efficiency_second_harmonic.pdf',  bbox_inches='tight')
+plt.savefig('./efficiency_second_harmonic.png',  bbox_inches='tight')
