96d97d716df84fd6d927a1b3dfddee51c6a27b46
beckstev <beckstev@web.de>
2019-05-16 17:01:30 +0200

    added plot for second harmonic efficincy

---
+++ nonlinear_optics/presentation_elements/phase_matching/efficiency_plot/I_plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def I(delta_k):
+    return (np.sin( 2 * delta_k))**2 / ( 2 *delta_k )**2
+
+
+Dk = np.linspace(-2*np.pi, 2*np.pi, 1000)
+
+
+plt.figure(figsize=(16, 9))
+plt.plot(Dk, I(Dk))
+plt.ylabel(r'$\Gamma \, / \, \mathrm{a.u.} $')
+plt.xlabel(r'$\Delta k \, / \, \mathrm{a.u.} $')
+
+plt.savefig('./efficiency_second_harmonic.pdf',  bbox_inches='tight')
