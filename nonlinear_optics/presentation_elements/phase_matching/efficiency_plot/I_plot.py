c58558478b9787fc8ababd7995162596fb3fd047
beckstev <beckstev@web.de>
2019-05-16 17:13:57 +0200

    further modifications

--- nonlinear_optics/presentation_elements/phase_matching/efficiency_plot/I_plot.py
+++ nonlinear_optics/presentation_elements/phase_matching/efficiency_plot/I_plot.py
@@ -10,8 +10,10 @@ Dk = np.linspace(-2*np.pi, 2*np.pi, 1e5)
 
 
 plt.figure(figsize=(16, 9))
-plt.plot(Dk, I(Dk))
-plt.ylabel(r'$\Gamma \, / \, \mathrm{a.u.} $')
-plt.xlabel(r'$\Delta k \, / \, \mathrm{a.u.} $')
+plt.plot(Dk, I(Dk), linewidth=4)
+
+plt.ylabel(r'$\Gamma \, / \, \mathrm{a.u.} $', fontsize=20)
+plt.xlabel(r'$\Delta k \, / \, \mathrm{a.u.} $', fontsize=20)
+plt.tick_params(axis='both', labelsize=20)
 
 plt.savefig('./efficiency_second_harmonic.png',  bbox_inches='tight')
